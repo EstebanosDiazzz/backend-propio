514e84663091c4d14347fece80017e36905e8d5d
EstebanosDiazzz <diazjuanesteban3@gmail.com>
2025-04-06 02:25:11 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,7 +9,6 @@ from app.controlador.PatientCrud import (
     DeleteMedicationAdministration,
 )
 
-            
 app = FastAPI()
 
 # CORS Middleware
@@ -20,9 +19,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-  # Permitir todos los encabezados
-
 
 # Endpoints para Patient
 @app.get("/patient/{patient_id}", response_model=dict)
@@ -55,10 +51,20 @@ async def get_medication_administration_by_id(med_admin_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
-
 @app.post("/medication_administration", response_model=dict)
 async def add_medication_administration(request: Request):
     new_med_admin_dict = await request.json()
+
+    # Asegúrate de que la referencia esté correctamente formateada
+    if 'performer' in new_med_admin_dict:
+        for performer in new_med_admin_dict['performer']:
+            if 'actor' in performer and 'reference' in performer['actor']:
+                # Formatear correctamente el campo 'reference' como un objeto 'Reference'
+                performer['actor']['reference'] = {
+                    "reference": performer['actor']['reference']
+                }
+
+    # Ahora procesar la administración de medicamentos
     status, med_admin_id = await WriteMedicationAdministration(new_med_admin_dict)
     if status == 'success':
         return {"_id": med_admin_id}
